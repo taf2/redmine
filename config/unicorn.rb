30dfa4eba2c256ace9e9a1639e6aaea1ba6cf0a5
Todd Fisher <taf2@spin.localdomain>
2009-12-14 08:40:58 -0500

    update signals to fire after fork

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,25 +27,6 @@ end
 
 
 before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
 end
 
 
@@ -60,27 +41,33 @@ after_fork do |server, worker|
   # Redis and Memcached would go here but their connections are established
   # on demand, so the master never opens a socket
 
-
   ##
-  # Unicorn master is started as root, which is fine, but let's
-  # drop the workers to git:git
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
 
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'deployer', 'deployer'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if RAILS_ENV == 'development'
-      STDERR.puts "couldn't change user, oh well"
-    else
-      raise e
+  old_pid = '/var/www/apps/bugs/shared/pids/unicorn.pid.oldbin'
+  #File.open(logfile, "a") {|f| f << "[#{Process]: check for old pid: #{old_pid} == #{server.pid}\n" } 
+
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      # make sure we don't send the winch and quit if we are the new master
+      #File.open(logfile, "a") {|f| f <<  "[#{Process]: check pids: #{File.read(old_pid).to_i} - #{Process.pid}\n" } 
+      if File.read(old_pid).to_i != Process.pid
+        Process.kill("WINCH", File.read(old_pid).to_i)
+        #File.open(logfile, "a") {|f| f << "[#{Process]: Sent old master the WINCH\n" } 
+        sleep 0.1 # pause to give the master a second to register the winch
+        Process.kill("QUIT", File.read(old_pid).to_i)
+        #File.open(logfile, "a") {|f| f << "[#{Process]: Sent old master the QUIT\n" } 
+      end
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
     end
   end
 end
